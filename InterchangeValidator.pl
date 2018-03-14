f58e576b7dcbdd61f59173e4c5681f3b1ae71c72
Daniel Lawson <lawson.vectorbase@gmail.com>
2018-03-14 17:41:51 +0000

    added tests for consistent collection metadata and checks for VBsp taxonomy definitions to catch potential typos

--- InterchangeValidator.pl
+++ InterchangeValidator.pl
@@ -17,12 +17,22 @@ my $help;
 my $summary;
 my %species;		# Hash of species present in interchange file
 
+my %collection_date;	# Hash of collection dates
+my %collection_desc;	# Hash of collection descriptions
+my %collection_protocol;	# Hash of collection protocols (traap type)
+my %VBsp;			# Hash of valid VBsp names and accessionss
 my $rows;			# Count of rows in file
 my $errors;  		# Number of errors found in file
 my $warnings;		# Count of warnings
 my $data_rows;		# Count of data rows
 my $comments;		# Count of comment rows
 my $blanks;		# Count of blank rows
+
+##
+## Parameters for validation checks
+##
+
+my $expected_columns = 21;		# Number of columns expected in the interchange format.
 
 
 #---------------------------------------------------------#
@@ -48,14 +58,13 @@ unless ( $file ) {
 	exit(0);
 }
 
+
+&parse_spreadsheet;
+
+
 # Assign error output filename
 my $output = $file . "_output.txt";
 
-##
-## Parameters for validation checks
-##
-
-my $expected_columns = 21;		# Number of columns expected in the interchange format.
 
 open (OUTPUT, "> $output");
 
@@ -68,16 +77,11 @@ while (my $line = <$data>) {
 	chomp $line;
 	$rows++;
 
+	# Ignore column headings (first line of a valid file)
+	next if ( $rows == 1);
+
 	if ($csv->parse($line)) {
 		my @f = $csv->fields();
-
-
-		##
-		##
-		##
-		if ( $f[5] eq "GPS_lat") {
-			next;
-		}
 
 		##
 		## Ignore column constraint for comment lines
@@ -124,14 +128,51 @@ while (my $line = <$data>) {
 		}
 
 		##
+		## Consistency of collection information
+		##
+		if ( ! defined $collection_date{$f[0]} ) {
+			$collection_date{$f[0]} = $f[2] . "-" . $f[3];
+		}
+		elsif ( $collection_date{$f[0]} ne "$f[2]-$f[3]" ) {
+			print OUTPUT "// ERROR: Collection $f[0] has multiple dates [$collection_date{$f[0]} v $f[2]-$f[3]]\n";
+			$errors++;
+		}
+
+		if ( ! defined $collection_desc{$f[0]} ) {
+			$collection_desc{$f[0]} = $f[7];
+		}
+		elsif ( $collection_desc{$f[0]} ne $f[7] ) {
+			print OUTPUT "// ERROR: Collection $f[0] has multiple descriptions [\"$collection_desc{$f[0]}\" v \"$f[7]]\"\n";
+			$errors++;
+		}
+
+		if ( ! defined $collection_protocol{$f[0]} ) {
+			$collection_protocol{$f[0]} = $f[8];
+		}
+		elsif ( $collection_protocol{$f[0]} ne $f[8] ) {
+			print OUTPUT "// ERROR: Collection $f[0] has multiple protocols (trap type) [\"$collection_protocol{$f[0]}\" v \"$f[8]\"]\n";
+			$errors++;
+		}
+
+
+		##
+		## Check validity of the species assingment
+		##
+		if ( (! defined $VBsp{$f[12]}) && ( $f[12] ne "BLANK") ) {
+			print OUTPUT "// WARNING: Asserted species $f[12] does not have a VBsp accession\n";
+			$warnings++;
+		}
+
+
+		##
 		## Sample size must be an integer (warn if zero?)
 		##
 		if ( $f[16] eq "" ) {
-			print OUTPUT "// ERROR: Collected sample size is absent (Species $f[12]) in line $rows \n";
+			print OUTPUT "// ERROR: Collected sample size is absent ($f[1] => Species $f[12]) in line $rows \n";
 			$errors++;
 		}
 		elsif ( $f[16] == 0 ) {
-			print OUTPUT "// WARNING: Collected sample size is zero (Species: $f[12]) in line $rows \n";
+			print OUTPUT "// WARNING: Collected sample size is zero ($f[1] => Species: $f[12]) in line $rows \n";
 			$warnings++;
 		}
 
@@ -188,17 +229,33 @@ if ( $summary ) {
 
 	foreach my $i (sort keys %species) {
 		next if ($i eq "BLANK");
-		printf ("%-32s %4d\n", $i,$species{$i});
+		printf ("%-32s $VBsp{$i} %4d\n", $i,$species{$i});
 	}
 
 	if ( $species{BLANK} ) {
 		print "\nBLANK (no mosquitoes collected)   $species{BLANK}\n";
 	}
-
-
-
 }
 
+sub parse_spreadsheet {
+
+	my $key = "1Km3YHrBMtREyjPoR0TdOxCdxHjBEZzq0Sgfka_Db6Tw";
+	my $curlcommand = "https://docs.google.com/spreadsheets/d/".$key."/export?exportFormat=tsv";
+	my @f;
+
+	# Get species metadata via curl and process
+	#---------------------------------------------------------#
+
+	open (FILE, "curl --silent $curlcommand |") or print "// WARNING failed to open spreadsheet\n";
+	while (<FILE>) {
+		chomp;
+		@f = split/\t/;
+		$VBsp{$f[1]} = $f[0];
+#		print "$f[1] => $f[0]\n";
+	}
+	close FILE;
+	return (%VBsp);
+}
 
 
 =pod
